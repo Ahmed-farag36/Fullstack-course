3759315279d73e24ea036fb348f2d0a69ed05009
Ahmed-Farag36 <Faragdeveloper@gmail.com>
2019-10-29 21:19:29 +0200

    anecdotes

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,48 +2,62 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+
+  const displayRandomly = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    randomIndex !== selected ? setSelected(randomIndex) : displayRandomly(); // ensure that anecdote changed
+  };
+
+  const handleVote = () => {
+    const updatedAnecdotes = [...votes];
+    updatedAnecdotes[selected] += 1;
+    setVotes(updatedAnecdotes);
+  };
+
+  const checkMostVotes = () => {
+    return votes.indexOf([...votes].sort()[votes.length - 1]);
+  };
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="Good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-      <h1>Statistics</h1>
-      {good + neutral + bad !== 0 ? (
-        <Statistics good={good} neutral={neutral} bad={bad} />
-      ) : (
-        <div>No feeback given</div>
-      )}
+      <Anecdote
+        header="Anecdote of the day"
+        anecdote={anecdotes[selected]}
+        votesCount={votes[selected]}
+      />
+      <Button handleClick={handleVote} text="Vote" />
+      <Button handleClick={displayRandomly} text="Next anecdote" />
+      <Anecdote
+        header="Anecdote with most votes"
+        anecdote={anecdotes[checkMostVotes()]}
+        votesCount={votes[checkMostVotes()]}
+      />
     </div>
   );
 };
+
+const Anecdote = ({ header, anecdote, votesCount }) => (
+  <>
+    <h1>{header}</h1>
+    <hr />
+    <h3>{anecdote}</h3>
+    <div>has {votesCount} votes</div>
+  </>
+);
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const Statistics = ({ good, neutral, bad }) => (
-  <table>
-    <tbody>
-      <Statistic text="Good" value={good} />
-      <Statistic text="Neutral" value={neutral} />
-      <Statistic text="Bad" value={bad} />
-      <Statistic text="All" value={good + neutral + bad} />
-      <Statistic text="Average" value={(good - bad) / (good + neutral + bad)} />
-      <Statistic text="Positive" value={`${good / (good + neutral + bad) * 100}%`} />
-    </tbody>
-  </table>
-);
-
-const Statistic = ({ text, value }) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-);
+const anecdotes = [
+  "If it hurts, do it more often",
+  "Adding manpower to a late software project makes it later!",
+  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+  "Premature optimization is the root of all evil.",
+  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it."
+];
 
 ReactDOM.render(<App />, document.getElementById("root"));
